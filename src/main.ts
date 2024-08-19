664cfff4ef0669213f62ee052fa8d4bade1e9ccd
xjaloldinov <xjaloldinov@texnomart.uz>
2024-08-19 11:15:11 +0500

    Added store

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import { createPinia } from "pinia";
+
 import "./assets/css/tailwind.css";
 
 import router from "./router";
 
 const app = createApp(App);
 
+app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
---
+++ src/stores/index.ts
@@ -0,0 +1,21 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+const STORE = "store";
+
+export const useStore = defineStore(STORE, () => {
+  return {};
+});
+
+const store = useStore();
+store.$subscribe((mutation) => {
+  if (mutation.storeId === STORE) {
+    sessionStorage.setItem(
+      STORE,
+      JSON.stringify({
+        // companies: store.companies,
+        // query: store.query,
+      })
+    );
+  }
+});
